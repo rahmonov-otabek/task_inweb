3015ee16cdf0d46f7793e8a761857cd9f59c8744
Otabek <otabekrahmonovtest@gmail.com>
2024-05-17 12:20:57 +0500

    feat: Admin template

---
+++ resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html"> <img alt="image" src="/admin/assets/img/logo.png" class="header-logo" /> <span
+            class="logo-name">Otika</span>
+        </a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+          <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link"><i data-feather="monitor"></i><span>Pages</span></a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link"><i data-feather="monitor"></i><span>Categories</span></a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link"><i data-feather="monitor"></i><span>Products</span></a>
+        </li>
+        {{-- <li class="dropdown">
+            <a href="{{ route('admin.tags.index') }}" class="nav-link"><i data-feather="monitor"></i><span>Products</span></a>
+          </li> --}}
+      </ul>
+    </aside>
+  </div>
