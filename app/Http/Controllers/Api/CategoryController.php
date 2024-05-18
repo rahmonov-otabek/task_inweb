d66c071803b35e6a96881391c528afcd2fff143c
Otabek <otabekrahmonovtest@gmail.com>
2024-05-18 17:14:09 +0500

    feat: API Category CRUD

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
- 
+namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;  
@@ -10,21 +10,14 @@ use App\Helpers\UploadHelper;
 
 class CategoryController extends Controller
 {
-   /**
+    /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $categories = Category::paginate(5); 
-        return view('admin.categories.index', compact('categories'));
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('admin.categories.create');
+        return response()->json($categories);
     }
 
     /**
@@ -38,7 +31,7 @@ class CategoryController extends Controller
 
         $category = Category::create($validated);
 
-        return redirect()->route('admin.categories.index')->with('success', 'Category created successfully'); 
+        return response()->json($category, 201);
     }
 
     /**
@@ -46,15 +39,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        return view('admin.categories.show', compact('category')); 
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Category $category)
-    {
-        return view('admin.categories.edit', compact('category'));
+        return response()->json($category);
     }
 
     /**
@@ -73,18 +58,18 @@ class CategoryController extends Controller
 
         $category->update($validated);
 
-        return redirect()->route('admin.categories.index')->with('success', 'Category updated successfully'); 
+        return response()->json($category);
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Category $category)
-    { 
+    {
         UploadHelper::deleteOldImage($category->image, 'category'); 
 
         $category->delete();
         
-        return redirect()->route('admin.categories.index')->with('success', 'Category deleted successfully'); 
+        return response()->json(null, 204);
     }
 }
